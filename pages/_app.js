b802d66bfaf63066edad16a5b34ac0c0e7bca1fe
Ritwik <sritwik03@gmail.com>
2022-06-09 19:24:11 +0530

    bug fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,8 @@
 import '../styles/globals.css'
-import ErrorBoundary from '../components/ErrorBoundary'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ErrorBoundary >
-      <Component {...pageProps} />
-    </ErrorBoundary>
+    <Component {...pageProps} />
   )
 }
 
